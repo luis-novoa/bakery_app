bbc7ef82e15d0ec2f3a5544b40250b846442a528
Luis Novoa <luis.matte.novoa@gmail.com>
2020-05-05 22:21:10 -0400

    Adds validation for the user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}, format: { with: ConstantData::VALID_EMAIL_REGEX }, if: :method_name?
-  
-  
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 2, maximum: 20}
 end
