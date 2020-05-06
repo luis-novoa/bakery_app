4ecce7a490bd4b7bde4a9de5d2ed84a0f4c44e3b
Luis Novoa <luis.matte.novoa@gmail.com>
2020-05-05 23:09:50 -0400

    Adds validations for the password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 2, maximum: 20}
+  validates :password, presence: true, length: {minimum: 6, maximum: 20}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,26 +3,56 @@ require 'spec_helper'
 
 RSpec.describe User, type: :model do
   let(:test_user) { User.new }
-  let(:test_user2) { User.new(name: 'test') }
+  let(:test_user2) { User.new(name: 'test', password: '123456') }
 
-  describe 'validations' do
+  def initial_user_setup
+    test_user.name = 'test'
+    test_user.password = 'HelloWorld'
+  end
+  
+
+  describe 'name validations' do
     it 'check for name presence' do
+      initial_user_setup
+      test_user.name = ''
       expect(test_user.save).to eq(false)
     end
 
     it "check for name's minimum characters" do
+      initial_user_setup
       test_user.name = 'a'
       expect(test_user.save).to eq(false)
     end
 
     it "check for name's maximum characters" do
+      initial_user_setup
       test_user.name = 'a' * 21
       expect(test_user.save).to eq(false)
     end
 
     it "check for name's uniqueness" do
-      test_user.name = 'test'
+      initial_user_setup
       test_user2.save
+      expect(test_user.save).to eq(false)
+    end
+  end
+
+  describe 'password validations' do
+    it 'check for password presence' do
+      initial_user_setup
+      test_user.password = ''
+      expect(test_user.save).to eq(false)
+    end
+
+    it "check for password's minimum characters" do
+      initial_user_setup
+      test_user.password = 'a'
+      expect(test_user.save).to eq(false)
+    end
+
+    it "check for password's maximum characters" do
+      initial_user_setup
+      test_user.password = 'a' * 21
       expect(test_user.save).to eq(false)
     end
   end
