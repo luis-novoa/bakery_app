f700a6fc345f3d1a27f571446c7f47b77647be78
Luis Novoa <luis.matte.novoa@gmail.com>
2020-05-12 15:44:38 -0400

    Adds FactoryBot to the project

--- spec/views/users/new_user_spec.rb
+++ spec/views/users/new_user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe 'users/new.html.erb', type: :feature do
-  context "page structure" do
-    visit new_user_path
-  end
-end
+# RSpec.describe 'users/new.html.erb', type: :feature do
+#   context "page structure" do
+#     visit new_user_path
+#   end
+# end
