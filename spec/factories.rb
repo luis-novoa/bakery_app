6852f182e98d7f862d01fe4b643c29a53f925f3d
Luis Novoa <luis.matte.novoa@gmail.com>
2020-05-12 18:36:26 -0400

    Refactors tests to use shoulda matchers

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,20 +1,8 @@
 FactoryBot.define do
   factory :user do
-    trait :model do
-      name { 'model' }
-      password { '123456' }
-      email { 'model@test.com' }
-      admin { 'user' }
-    end
-
-    trait :dummy do
-      name { 'dummy' }
-      password { 'testest' }
-      email { 'test@test.com' }
-      admin { 'user' }
-    end
-
-    factory :model_user, traits: [:model]
-    factory :test_user, traits: [:dummy]
+    name { 'model' }
+    password { '123456' }
+    email { 'model@test.com' }
+    admin { 'user' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,64 +2,14 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  let(:test_user) { build(:test_user) }
-  let(:model_user) { create(:model_user) }
+  subject { build(:user) } 
 
-  def initial_user_setup
-    test_user.name = 'test'
-    test_user.password = 'HelloWorld'
-  end
-  
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  it { is_expected.to validate_length_of(:name).is_at_least(2).is_at_most(20) }
 
-  describe 'name validations' do
-    it 'check for name presence' do
-      initial_user_setup
-      test_user.name = ''
-      expect(test_user.save).to eq(false)
-    end
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_length_of(:password).is_at_least(6).is_at_most(20) }
 
-    it "check for name's minimum characters" do
-      initial_user_setup
-      test_user.name = 'a'
-      expect(test_user.save).to eq(false)
-    end
-
-    it "check for name's maximum characters" do
-      initial_user_setup
-      test_user.name = 'a' * 21
-      expect(test_user.save).to eq(false)
-    end
-
-    it "check for name's uniqueness" do
-      initial_user_setup
-      expect(test_user.save).to eq(false)
-    end
-  end
-
-  describe 'password validations' do
-    it 'check for password presence' do
-      initial_user_setup
-      test_user.password = ''
-      expect(test_user.save).to eq(false)
-    end
-
-    it "check for password's minimum characters" do
-      initial_user_setup
-      test_user.password = 'a'
-      expect(test_user.save).to eq(false)
-    end
-
-    it "check for password's maximum characters" do
-      initial_user_setup
-      test_user.password = 'a' * 21
-      expect(test_user.save).to eq(false)
-    end
-  end
-
-  describe "admin validations" do
-    it "check for presence" do
-      initial_user_setup
-      expect(test_user.save).to eq(false) 
-    end
-  end
+  it { is_expected.to validate_presence_of(:admin) }
 end
