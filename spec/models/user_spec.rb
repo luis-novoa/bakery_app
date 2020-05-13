5a03a64caa8ddccd6a642f104352a489a5c8864e
Luis Novoa <luis.matte.novoa@gmail.com>
2020-05-13 11:29:57 -0400

    Creates tests for user.email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to validate_length_of(:name).is_at_least(2).is_at_most(20) }
 
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to allow_value("email@test.com").for(:email) } 
+  it { is_expected.to_not allow_value("emailtest.com").for(:email) }
+
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to validate_length_of(:password).is_at_least(6).is_at_most(20) }
 
@@ -23,7 +28,5 @@ RSpec.describe User, type: :model do
       test.save
       expect(test.encrypted_password).to_not eq(model.encrypted_password)
     end
-    
   end
-  
 end
