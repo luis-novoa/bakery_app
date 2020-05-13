0da2f272375f7c02ad08ccead375cf7fe1e66832
Luis Novoa <luis.matte.novoa@gmail.com>
2020-05-13 10:53:13 -0400

    Refactors password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 6..20
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 2, maximum: 20}
-  validates :password, presence: true, length: { minimum: 6, maximum: 20 }
   validates :admin, presence: true, inclusion: { in: %w(super admin user) }
 end
