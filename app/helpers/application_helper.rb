e264fd82e1221680ab084ccf9d9cf61ea074b01d
Luis Novoa <luis.matte.novoa@gmail.com>
2020-05-14 15:00:37 -0400

    Adds links to edit user info and log out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def user_utilities
     if user_signed_in?
-      'nothing'
+      link_to('Edit Your Info', edit_user_registration_path) +
+      link_to('Log Out', destroy_user_session_path)
     else
       link_to('Sign Up', new_user_registration_path) +
       link_to('Log In', new_user_session_path)
--- spec/views/application/app_wide_spec.rb
+++ spec/views/application/app_wide_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe 'application-wide features |', type: :feature do
 
     it { is_expected.to have_link 'Sign Up', href: new_user_registration_path }
     it { is_expected.to have_link 'Log In', href: new_user_session_path }
+    it { is_expected.to have_no_link 'Log Out', href: destroy_user_session_path }
+    it { is_expected.to have_no_link 'Edit Your Info', href: edit_user_registration_path }
   end
 
   context "page for logged user" do
@@ -23,5 +25,7 @@ RSpec.describe 'application-wide features |', type: :feature do
 
     it { is_expected.to have_no_link 'Sign Up', href: new_user_registration_path }
     it { is_expected.to have_no_link 'Log In', href: new_user_session_path }
+    it { is_expected.to have_link 'Log Out', href: destroy_user_session_path }
+    it { is_expected.to have_link 'Edit Your Info', href: edit_user_registration_path }
   end
 end
